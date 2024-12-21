bc0922d35de6453ebb58533ccee659206c985924
Will Alicar <will@alicar.me>
2024-12-21 14:01:24 -0800

    init

---
+++ main.js
@@ -0,0 +1,21 @@
+// utils
+function createShader(gl, type, source) {
+    let shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    let success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (success) return shader;
+    console.log(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+}
+
+function createProgram(gl, vertShader, fragShader) {
+    let program = gl.createProgram();
+    gl.attachShader(program, vertShader);
+    gl.attachShader(program, fragShader);
+    gl.linkProgram(program);
+    let success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) return program;
+    console.log(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+}
