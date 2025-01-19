317bf7405c9dcdb1f7a1c629e4d81eeb52201425
Will Alicar <will@alicar.me>
2025-01-19 11:45:22 -0800

    initalize plane

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
+import { mat4 } from "gl-matrix";
+
+const root = document.getElementById("snippet-1");
+
+// plane vertices
+const planeVertData = [
+    // bottom right
+    1.0, -1.0, 0.0,
+    // bottom left
+    -1.0, -1.0, 0.0,
+    // top left
+    -1.0, 1.0, 0.0,
+    // top right
+    1.0, 1.0, 0.0,
+]
+
 // plane shaders
-const vertSrc = `#verision 300 es
+const planeVertSrc = `#verision 300 es
 precision mediump float;
 in vec3 a_pos;
 uniform mat4 u_model;
@@ -11,7 +27,7 @@ void main() {
 }
 `;
 
-const fragSrc = `#version 300 es
+const planeFragSrc = `#version 300 es
 precision mediump float;
 
 void main() {
@@ -19,7 +35,56 @@ void main() {
 }
 `;
 
-function main() {};
+function main() {
+    // make program and compile shaders
+    const canvas = document.createElement("canvas");
+    root.appendChild(canvas);
+    const gl = canvas.getContent("webgl2");
+    if (!gl) {
+        canvas.textContent = "WebGL is not supported";
+        return;
+    }
+
+    // make program and compile shaders
+    const planeVertShader = createShader(gl, gl.VERTEX_SHADER, planeVertSrc);
+    const planeFragShader = createShader(gl, gl.FRAGMENT_SHADER, planeFragSrc);
+    const planeProgram = createProgram(gl, planeVertShader, planeFragShader);
+
+    // make VBOs, and VAOs
+    const planeVao = gl.createVertexArray();
+    gl.bindVertexArray(planeVao);
+
+    let buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(planeVertData), gl.STATIC_DRAW);
+
+    let posAttribLoc = gl.getAttribLocation(planeProgram, "a_pos");
+    gl.enableVertexAttribArray(posAttribLoc);
+    gl.vertexAttribPointer(posAttribLoc, 3, gl.FLOAT, false, 3 * 4, 0);
+
+    const modelUniformLocPlane = gl.getUniformLocation(planeProgram, "u_model");
+    const viewUniformLocPlane = gl.getUniformLocation(planeProgram, "u_view");
+    const projUniformLocPlane = gl.getUniformLocation(planeProgram, "u_proj");
+
+    // start drawing
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    gl.clearColor(0,0,0,0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.enable(gl.DEPTH_TEST);
+    gl.useProgram(program);
+
+    let model = mat4.create();
+    let view = mat4.create();
+    mat4.lookAt(view, [1,3,0], [0,0,0], [0,1,0]);
+
+    let proj = mat4.create();
+    mat4.perspective(proj, Math.PI/4, canvas.width/canvas.height, 0.1, 100);
+
+    function loop() {
+        // @todo
+    }
+
+};
 
 function makePath() {};
 
