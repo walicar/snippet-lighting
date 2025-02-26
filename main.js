839d589b8f6f4e7d42fd703f34bcb839629feb18
Will Alicar <will@alicar.me>
2025-02-25 21:21:01 -0800

    wrap main() around a function

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import { mat4 } from "gl-matrix";
 
+function run() {
+
 const root = document.getElementById("lighting-1");
 root.style.position = "relative";
 root.style.height = "500px";
@@ -32,9 +34,9 @@ const cubeVertData = [
 ];
 
 const cubeIndices = [
-    0, 1, 2,  0, 2, 3,      // top face
-    4, 5, 6,  4, 6, 7,      // bottom face
-    8, 9, 10,  8, 10, 11,   // front face
+    0,  1,   2,  0,  2,  3,      // top face
+    4,  5,   6,  4,  6,  7,      // bottom face
+    8,  9,  10,  8, 10, 11,   // front face
     12, 13, 14, 12, 14, 15, // back face
     16, 17, 18, 16, 18, 19, // left face
     20, 21, 22, 20, 22, 23  // right face
@@ -229,3 +231,6 @@ function setupUI(root) {
 }
 
 main();
+}
+
+run();
